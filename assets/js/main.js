e25844c19512dabd1b90cab348fe42601b89cbdb
SaheleeParveen <saheleeparveen4@gmail.com>
2025-02-22 03:04:59 +0530

    upload file

---
+++ assets/js/main.js
@@ -0,0 +1,59 @@
+window.addEventListener("scroll", function () {
+    var header = document.querySelector("header");
+    if (window.scrollY > 50) {
+        header.classList.add("stickyheader");
+    } else {
+        header.classList.remove("stickyheader");
+    }
+});
+
+
+const panels = document.querySelectorAll('.panel')
+
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+        removeActiveClasses()
+        panel.classList.add('active')
+    })
+})
+
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    })
+}
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    var splide = new Splide("#property", {
+      type: "loop",
+      arrows: true,
+      gap: 0,
+      perPage: 1,
+      pagination: true,
+      focus: "start",
+      autoplay: false,
+      speed: 800,
+      interval: 3000,
+      rewind: true,
+      rewindSpeed: 500,
+      breakpoints: {
+        1024: { perPage: 1 },
+        768: { perPage: 1 },
+        400: { perPage: 1 },
+      },
+    });
+  
+    let progressBar = document.getElementById("progress-bar");
+  
+    splide.on("move", function () {
+      let totalSlides = splide.length - 1;
+      let currentSlide = splide.index;
+      let progress = ((currentSlide + 1) / (totalSlides + 1)) * 100;
+  
+      progressBar.style.width = progress + "%";
+    });
+  
+    splide.mount();
+  });
+  
